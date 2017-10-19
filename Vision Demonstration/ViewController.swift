fad1b6daf009041827026e1ffe683ee616e6f238
bergmpe <bergmpe@gmail.com>
2017-10-19 12:27:05 -0200

    Initial Commit

---
+++ Vision Demonstration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Vision Demonstration
+//
+//  Created by padrao on 19/10/17.
+//  Copyright © 2017 padrao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
